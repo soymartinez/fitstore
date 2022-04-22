b55f5eb0fd4502394e7d8501e53f65e44b4aef15
omgismartinez <martnzomg@gmail.com>
2022-04-22 17:47:25 -0500

    API

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ 
+    name: 'Alvaro Martinez',
+    age: 21,
+    hobbies: ['coding', 'creation', 'music']
+  })
 }
