5332107c97005b1915209323ee448eb4d4b91a98
omgismartinez <martnzomg@gmail.com>
2022-05-27 23:21:03 -0500

    fix: hex ramdomcolor

--- styles/utils.js
+++ styles/utils.js
@@ -1,4 +1,8 @@
 export const randomColor = () => {
     const color = Math.floor(Math.random() * 16777215).toString(16);
-    return '#'+color;
+    if (color.length === 6) {
+        return `#${color}`;
+    } else {
+        return randomColor();
+    }
 }
