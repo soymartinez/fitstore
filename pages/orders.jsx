cd077e7996ed2238fadf34543073c30af59fe1ed
omgismartinez <martnzomg@gmail.com>
2022-05-27 23:22:51 -0500

    feat: layout / padding

--- pages/orders.jsx
+++ pages/orders.jsx
@@ -1,16 +1,15 @@
 import Link from 'next/link'
-import Navbar from '../components/navbar'
+import Layout from '../components/layout'
 
 export default function Pedidos() {
     return (
-        <>
-            <Navbar />
-            <div className='pt-24 container lg:px-32 md:px-12 px-8'>
+        <Layout title={'Orders'}>
+            <div className='pt-24 container lg:px-32 md:px-8 px-4'>
                 <h1 className='font-bold text-2xl'>
                     Pedidos
                 </h1>
                 <Link href="/">Go home</Link>
             </div>
-        </>
+        </Layout>
     )
 }
