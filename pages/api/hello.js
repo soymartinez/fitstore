5235a33f38db0098a66743bcdbdfa7c1c8de6cfd
omgismartinez <martnzomg@gmail.com>
2022-06-07 11:51:50 -0500

    rm: mocks api

--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ 
-    name: 'Alvaro Martinez',
-    age: 21,
-    hobbies: ['coding', 'creation', 'music']
-  })
-}
