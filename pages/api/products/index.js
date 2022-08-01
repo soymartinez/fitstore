53532da0eedd67381d839c17cd0fbff08de69877
omgismartinez <martnzomg@gmail.com>
2022-08-01 00:51:06 -0500

    🎯 fix: no products

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,28 +1,28 @@
-import Product from 'models/Product'
-import { dbConnect } from 'utils/mongoose'
+// import Product from 'models/Product'
+// import { dbConnect } from 'utils/mongoose'
 
-dbConnect();
+// dbConnect();
 
-export default async (req, res) => {
-  const { method, body } = req;
+// export default async (req, res) => {
+//   const { method, body } = req;
 
-  switch (method) {
-    case "GET":
-      try {
-        const products = await Product.find();
-        return res.status(200).json(products);
-      } catch (error) {
-        return res.status(400).json({ msg: error.message });
-      }
-    case "POST":
-      try {
-        const newProduct = new Product(body);
-        const savedProduct = await newProduct.save();
-        return res.status(201).json(savedProduct);
-      } catch (error) {
-        return res.status(400).json({ msg: error.message });
-      }
-    default:
-      return res.status(400).json({ msg: "This method is not supported" });
-  }
-};
+//   switch (method) {
+//     case "GET":
+//       try {
+//         const products = await Product.find();
+//         return res.status(200).json(products);
+//       } catch (error) {
+//         return res.status(400).json({ msg: error.message });
+//       }
+//     case "POST":
+//       try {
+//         const newProduct = new Product(body);
+//         const savedProduct = await newProduct.save();
+//         return res.status(201).json(savedProduct);
+//       } catch (error) {
+//         return res.status(400).json({ msg: error.message });
+//       }
+//     default:
+//       return res.status(400).json({ msg: "This method is not supported" });
+//   }
+// };
