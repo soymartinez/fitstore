d0d0db49f62b6f4bceb157d1a8866a1654424c9d
omgismartinez <martnzomg@gmail.com>
2022-08-01 23:33:09 -0500

    🌌 refactor (id): prisma get / post

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -1,51 +1,37 @@
-// import { dbConnect } from 'utils/mongoose'
-// import Product from 'models/Product'
+import prisma from 'utils/prisma'
 
-// dbConnect();
+export default async (req, res) => {
+    const { method, query: { id } } = req
 
-// export default async (req, res) => {
-//   const {
-//     method,
-//     query: { id }
-//   } = req;
-
-//   switch (method) {
-//     case "GET":
-//       try {
-//         const product = await Product.findById(id);
-//         if (!product) return res.status(404).json({ msg: "Product does not exxists" });
-//         return res.status(200).json(product);
-//       } catch (error) {
-//         return res.status(400).json({ msg: error.message });
-//       }
-//       case "PUT":
-//         try {
-//           const product = await Product.findByIdAndUpdate(id, req.body, {
-//             new: true,
-//             runValidators: true,
-//           });
-//           if (!product) {
-//             return res.status(400).json({ success: false });
-//           }
-//           res.status(200).json({ success: true, data: product });
-//         } catch (error) {
-//           res.status(400).json({ success: false });
-//         }
-//         break;
-  
-//       case "DELETE":
-//         try {
-//           const product = await Task.deleteOne({ _id: id });
-//           if (!product) {
-//             return res.status(400).json({ success: false });
-//           }
-//           res.status(200).json({ success: true, data: {} });
-//         } catch (error) {
-//           res.status(400).json({ success: false });
-//         }
-//         break;
-//       default:
-//         res.status(400).json({ success: false });
-//         break;
-//     }
-// };
+    switch (method) {
+        case 'GET':
+            try {
+                const product = await prisma.products.findUnique({ where: { id } })
+                if (!product) return res.status(404).json({ msg: 'Product does not exists' })
+                return res.status(200).json(product)
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        case 'UPDATE':
+            try {
+                const updated = await prisma.products.update({
+                    where: { id },
+                    data: { ...body, }
+                })
+                if (!updated) return res.status(400).json({ msg: error.message })
+                return res.status(200).json({ success: true, data: product })
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        case 'DELETE':
+            try {
+                const deleted = await prisma.products.delete({ where: { id } })
+                if (!deleted) return res.status(400).json({ msg: error.message })
+                res.status(200).json({ success: true, data: {} })
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        default:
+            return res.status(400).json({ msg: 'This method is not supported' })
+    }
+}
