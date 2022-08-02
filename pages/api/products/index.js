5a74b50f062b415cbd50c1e595fcc9b2dc467f8d
omgismartinez <martnzomg@gmail.com>
2022-08-01 23:33:42 -0500

    🌌 refactor (index): prisma get / post

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,28 +1,36 @@
-// import Product from 'models/Product'
-// import { dbConnect } from 'utils/mongoose'
+import prisma from 'utils/prisma'
 
-// dbConnect();
+export default async (req, res) => {
+    const { method, body } = req
 
-// export default async (req, res) => {
-//   const { method, body } = req;
-
-//   switch (method) {
-//     case "GET":
-//       try {
-//         const products = await Product.find();
-//         return res.status(200).json(products);
-//       } catch (error) {
-//         return res.status(400).json({ msg: error.message });
-//       }
-//     case "POST":
-//       try {
-//         const newProduct = new Product(body);
-//         const savedProduct = await newProduct.save();
-//         return res.status(201).json(savedProduct);
-//       } catch (error) {
-//         return res.status(400).json({ msg: error.message });
-//       }
-//     default:
-//       return res.status(400).json({ msg: "This method is not supported" });
-//   }
-// };
+    switch (method) {
+        case 'GET':
+            try {
+                const products = await prisma.product.findMany()
+                return res.status(200).json(products)
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        case 'POST':
+            try {
+                const product = await prisma.product.create({
+                    data: {
+                        name: body.name,
+                        subname: body.subname,
+                        brand: body.brand,
+                        image: body.image,
+                        descriptions: {
+                            create: [
+                                { ...body.descriptions }
+                            ]
+                        }
+                    }
+                })
+                return res.status(201).json(product)
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        default:
+            return res.status(400).json({ msg: 'This method is not supported' })
+    }
+}
