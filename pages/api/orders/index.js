b50a55a9a8c2d2ba6b951d716608ddca9a47b0d1
omgismartinez <martnzomg@gmail.com>
2022-08-03 23:18:12 -0500

    🌌 refactor (orders): comment code

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,16 +1,16 @@
 export const data = {
     "orders": [
-        {
-            "id": 1,
-            "name": "MASS TECH Elite",
-            "brand": "Muscletech",
-            "weight": 2.26,
-            "toast": "vainilla",
-            "price": 998.00,
-            "amount": 1,
-            "description": "This is a product description, it can be as long or short as you want it to be. It can even be empty.",
-            "image": "https://firebasestorage.googleapis.com/v0/b/fitstore-4db57.appspot.com/o/muscletech-mass-tech-elite-300x300.webp?alt=media&token=7b6bc615-070c-4c07-973e-b7545a665659"
-        },
+        // {
+        //     "id": 1,
+        //     "name": "MASS TECH Elite",
+        //     "brand": "Muscletech",
+        //     "weight": 2.26,
+        //     "toast": "vainilla",
+        //     "price": 998.00,
+        //     "amount": 1,
+        //     "description": "This is a product description, it can be as long or short as you want it to be. It can even be empty.",
+        //     "image": "https://firebasestorage.googleapis.com/v0/b/fitstore-4db57.appspot.com/o/muscletech-mass-tech-elite-300x300.webp?alt=media&token=7b6bc615-070c-4c07-973e-b7545a665659"
+        // },
         {
             "id": 2,
             "name": "Best Protein Nutrition Pro Gainer",
