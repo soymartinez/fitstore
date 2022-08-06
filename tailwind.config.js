606a90c221995c96de4ece6c237e56544dde41fb
omgismartinez <martnzomg@gmail.com>
2022-08-06 17:12:14 -0500

    🌌 theme: shimmer keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,11 @@ module.exports = {
   ],
   theme: {
     extend: {},
-  },
-  keyframes: {
-    shimmer: {
-      '100%': {
-        transform: 'translateX(100%)',
+    keyframes: {
+      shimmer: {
+        '100%': {
+          transform: 'translateX(100%)',
+        },
       },
     },
   },
