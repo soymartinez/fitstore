1fe24364f1f660ce77533c2d28c171b62d488ef4
omgismartinez <martnzomg@gmail.com>
2022-08-06 23:10:48 -0500

    🌌 refactor (navbar): useEffect, design and skeleton

--- components/navbar.js
+++ components/navbar.js
@@ -1,56 +1,61 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import { getSession } from 'next-auth/react'
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Icon from './icon';
+import { getSession, useSession } from 'next-auth/react'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Skeleton from './skeleton'
 
-export default function Navbar({ status }) {
-    const router = useRouter();
-    const [scroll, setScroll] = useState(false);
-    const [session, setSession] = useState(null);
+export default function Navbar() {
+    const router = useRouter()
+    const [scroll, setScroll] = useState(false)
+    const [session, setSession] = useState(null)
+    const { status } = useSession()
     useEffect(() => {
-        (async () => {
+        async function Session() {
             const session = await getSession()
             setSession(session)
-        })()
+        }
+        Session()
         window.addEventListener('scroll', () => {
             if (window.scrollY > 0) {
-                setScroll(true);
+                setScroll(true)
             } else {
-                setScroll(false);
+                setScroll(false)
             }
-        });
-        window.scrollY > 0 ? setScroll(true) : setScroll(false)
-    }, []);
+        })
+    }, [scroll])
 
     return (
         <>
             <nav className={`fixed border-b border-transparent transition-colors duration-300 py-5 top-0 inset-x-0 z-50 
                             ${scroll ? 'scrolled ease-in' : 'ease-out'}`}>
-                <div className="container lg:px-32 md:px-8 px-4 flex justify-between items-center">
+                <div className='container lg:px-32 md:px-8 px-4 flex justify-between items-center'>
                     <Link href={'/'}>
-                        <a className="flex items-center">
-                            <Link href={'/'}>
-                                <span className='name cursor-pointer'>
-                                    Fitstore
-                                </span>
-                            </Link>
+                        <a className='flex items-center'>
+                            <span className='name cursor-pointer'>
+                                Fitstore
+                            </span>
                         </a>
                     </Link>
                     {
                         status === 'loading' ?
                             <div className='flex items-center ease-out duration-500'>
                                 <div className='mx-5 md:mx-10'>
-                                    <h2 className='text-gray-700 bg-gray-700 animate-pulse rounded-full'>
-                                        pedidos
-                                    </h2>
+                                    <Skeleton rounded='rounded-full'>
+                                        <h2 className='text-gray-700 bg-gray-700 animate-pulse rounded-full'>
+                                            pedidos
+                                        </h2>
+                                    </Skeleton>
                                 </div>
                                 <h2 className='md:w-24 ml-5 md:ml-10 flex justify-between items-center'>
-                                    <div className='w-10 h-10 bg-gray-700 animate-pulse rounded-full' />
-                                    <div className='bg-gray-700 animate-pulse rounded-full'>
-                                        <span className='md:block hidden text-gray-700'>perfil</span>
-                                    </div>
+                                    <Skeleton rounded='rounded-full' background='bg-gray-700'>
+                                        <div className='w-10 h-10 bg-gray-700 animate-pulse rounded-full' />
+                                    </Skeleton>
+                                    <Skeleton rounded='rounded-full'>
+                                        <div className='bg-gray-700 animate-pulse'>
+                                            <span className='md:block hidden text-gray-700'>perfil</span>
+                                        </div>
+                                    </Skeleton>
                                 </h2>
                             </div>
                             : ''
@@ -58,10 +63,12 @@ export default function Navbar({ status }) {
                     {
                         status === 'unauthenticated' ?
                             <Link href={'/auth/signin'}>
-                                <h2 className={`cursor-pointer hover:text-white hover:animate-none animate-pulse
+                                <a>
+                                    <h2 className={`cursor-pointer hover:text-white hover:animate-none animate-pulse
                                             ${router.asPath == '/auth/signin' ? 'text-white animate-none' : ''}`}>
-                                    inicia sesión
-                                </h2>
+                                        inicia sesión
+                                    </h2>
+                                </a>
                             </Link>
                             : ''
                     }
@@ -85,8 +92,10 @@ export default function Navbar({ status }) {
                                             {
                                                 session
                                                     ? <Image src={session.user.image} className='rounded-full' width={40} height={40} alt={'profile'} />
-                                                    : <div className='w-10 h-10 bg-gray-700 animate-pulse rounded-full' />
-
+                                                    :
+                                                    <Skeleton rounded='rounded-full'>
+                                                        <div className='w-10 h-10 bg-gray-700 animate-pulse rounded-full' />
+                                                    </Skeleton>
                                             }
                                         </div>
                                         <span className='md:block hidden'>perfil</span>
