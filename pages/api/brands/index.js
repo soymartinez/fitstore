178cd48f11f441b7db4ade19e43fcfef86c039e3
omgismartinez <martnzomg@gmail.com>
2022-08-08 00:40:55 -0500

    🎯 fix (brands): Do not know how to serialize a BigInt

--- pages/api/brands/index.js
+++ pages/api/brands/index.js
@@ -1,5 +1,9 @@
 import prisma from 'utils/prisma'
 
+BigInt.prototype.toJSON = function () {
+    return this.toString()
+}
+
 export default async (req, res) => {
     const brands = await prisma.brand.findMany()
     return res.status(200).json(brands)
