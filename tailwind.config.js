15e7cdef0cbe48d10d97e952d7bd8e2acb7a6e87
omgismartinez <martnzomg@gmail.com>
2022-08-08 23:04:47 -0500

    🌌 refactor: animate-ping

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,19 @@ module.exports = {
     "./components/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        '.animate-ping': '1s cubic-bezier(0, 0, 0.2, 1) infinite',
+      }
+    },
     keyframes: {
       shimmer: {
-        '100%': {
-          transform: 'translateX(100%)',
+        '100%': { transform: 'translateX(100%)' },
+      },
+      ping: {
+        '75%, 100%': {
+          transform: 'scale(2)',
+          opacity: '0'
         },
       },
     },
