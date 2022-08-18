42b350ac715d27a31c1fcbc11240aecd3f6181cb
omgismartinez <martnzomg@gmail.com>
2022-08-17 23:38:53 -0500

    🌌 refactor (products/edit): route protection

--- pages/admin/products/edit/[id].jsx
+++ pages/admin/products/edit/[id].jsx
@@ -1,8 +1,28 @@
+import { unstable_getServerSession } from 'next-auth'
+import { authOptions } from 'pages/api/auth/[...nextauth]'
+
 import Layout from 'components/layout'
 import { fetcher } from 'lib/fetcher'
 import FormProduct from 'components/formproduct'
 
-export async function getServerSideProps({ params }) {
+export async function getServerSideProps({ params, req, res }) {
+    const session = await unstable_getServerSession(req, res, authOptions)
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/login',
+                permanent: false
+            }
+        }
+    } else if (session.user.role !== 'admin') {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     const { id } = params
     const product = await fetcher(`${process.env.API_URL}/products/${id}`)
     return {
