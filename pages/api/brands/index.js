91332315f71072ba081dab9db67b0de240027d89
omgismartinez <martnzomg@gmail.com>
2022-08-20 01:48:51 -0500

    🌌 refactor (brands): get, post

--- pages/api/brands/index.js
+++ pages/api/brands/index.js
@@ -5,6 +5,27 @@ BigInt.prototype.toJSON = function () {
 }
 
 export default async (req, res) => {
-    const brands = await prisma.brand.findMany()
-    return res.status(200).json(brands)
+    const { method } = req
+
+    switch (method) {
+        case 'GET':
+            try {
+                const brands = await prisma.brand.findMany()
+                return res.status(200).json(brands)
+            } catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        case 'POST':
+            try {
+                const brand = await prisma.brand.create({
+                    data: req.body,
+                })
+                return res.status(201).json(brand)
+            }
+            catch (error) {
+                return res.status(400).json({ msg: error.message })
+            }
+        default:
+            return res.status(400).json({ msg: 'Method not allowed' })
+    }
 }
